ea89fae6a8ce755e4327096d670eac198d348fb7
hugogomes2 <Hl522052@gmail.com>
2025-05-27 00:35:41 -0300

    Criacao de testes para validar a feature login

--- cypress/e2e/loja-ebac/login.cy.js
+++ cypress/e2e/loja-ebac/login.cy.js
@@ -1,13 +1,28 @@
-/// import cypress from "cypress"
+///import cypress from "cypress"
 
 describe("Feature login", () => {
-    it('most login successfully', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
+    });
+
+    it('most login successfully', () => {
         cy.get('#username').type('Teste400@teste.com.br');
         cy.get('#password').type('Senha12345*');
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, teste400 (não é teste400? Sair)')
     })
+    it('should show error message when entering incorrect user', () => {
+        cy.get('#username').type('Teste400@test.com.br');
+        cy.get('#password').type('Senha12345*');
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('should show error message when entering incorrect password', () => {
+        cy.get('#username').type('Teste400@teste.com.br');
+        cy.get('#password').type('Senha1234*');
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')
+    });
 })
 
